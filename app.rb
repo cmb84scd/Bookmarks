efb78d9f80d2133ad19458f41d6d8e1428be7c7a
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-05-06 16:39:53 +0100

    Wrapping Database Data in Program Objects

--- app.rb
+++ app.rb
@@ -15,8 +15,8 @@ class BookmarkManager < Sinatra::Base
     erb :"bookmarks/new"
   end
 
-  post '/bookmarks' do
-    Bookmark.create(url: params[:url])
+  post '/bookmarks/new' do
+    Bookmark.create(url: params[:url], title: params[:title])
     redirect '/bookmarks'
   end
 
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -4,14 +4,14 @@ feature 'bookmarks' do
 
   scenario 'viewing bookmarks url' do
     # Add the test data
-    Bookmark.create(url: 'http://www.makersacademy.com')
-    Bookmark.create(url: 'http://www.destroyallsoftware.com')
-    Bookmark.create(url: 'http://www.google.com')
+    Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Bookmark.create(url: 'http://www.destroyallsoftware.com', title: 'Destroy All Software')
+    Bookmark.create(url: 'http://www.google.com', title: 'Google')
 
     visit('/bookmarks')
 
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+    expect(page).to have_link('Destroy All Software',  href: 'http://www.destroyallsoftware.com')
+    expect(page).to have_link('Google', href: 'http://www.google.com')
   end
 end
