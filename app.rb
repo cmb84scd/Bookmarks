964ae93a24ab349dcb6cb3fb677b2a3b16614b8b
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-06 15:12:33 +0100

    added functionality to store bookmarks

--- app.rb
+++ app.rb
@@ -11,6 +11,17 @@ class BookmarkManager < Sinatra::Base
     erb :bookmarks
   end
 
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    url = params['url']
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    redirect '/bookmarks'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
